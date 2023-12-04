224088a1aab3fdcf59596157e2be57ea15109856
JoseCarlos67 <carlos6767jose@gmail.com>
2023-12-04 13:38:30 -0300

    Classe ContaBancaria criada

---
+++ Encapsulamento/ContaBancaria.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Encapsulamento
+{
+    public class ContaBancaria
+    {
+        private int _numero;
+        private string _titular;
+        private double _saldo;
+
+
+        public ContaBancaria(int numero, string titular){
+            _numero = numero;
+            _titular = titular;
+        }
+
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular) {
+            _saldo = saldo;
+        }
+
+        public override string ToString()
+        {
+            return "Conta "
+            + _numero
+            + ", Titular: "
+            + _titular
+            + ", Saldo: $"
+            + _saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
--- Encapsulamento/Program.cs
+++ Encapsulamento/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using Encapsulamento;
+
+class Program {
+    public static void Main(string[] args) {
+    
+        ContaBancaria conta;
+
+        Console.WriteLine("Informe os dados da conta:");
+
+        Console.Write("Entre com o número da conta: ");
+        int numero = int.Parse(Console.ReadLine());
+
+        Console.Write("Entre com o titular da conta: ");
+        string titular = Console.ReadLine();
+
+        Console.Write("Haverá depósito inicial (s/n)? ");
+        char resposta = Console.ReadLine()[0];
+        if (resposta == 's' || resposta == 'S') {
+            Console.Write("Entre com um valor para depósito: ");
+            double deposito = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            conta = new ContaBancaria(numero, titular, deposito);
+        } else {
+            conta = new ContaBancaria(numero, titular);
+        }
+
+        Console.WriteLine(conta);
+    }
+}
